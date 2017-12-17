febfa4bb3255a0061b921e49585e21adeddc2f52
Tine Jozelj <tine.jozelj@tjo.space>
2017-12-17 15:38:09 +0100

    fix - 1.0.2

--- lib/index.ts
+++ lib/index.ts
@@ -1,10 +1,9 @@
-const name = 'webpack-obj-loader';
-
 export default function (source) {
   this.cacheable();
 
   return `
   import { Mesh } from 'webgl-obj-loader';
-  const source = \`${source}\`;
-  export default new Mesh(source);`;
+  const source = '${JSON.stringify(source)}';
+  const mesh = new Mesh(source);
+  export default mesh;`;
 }
