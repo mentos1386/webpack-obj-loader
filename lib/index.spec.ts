2cd93c54b1eeea4b41ad6856a929f3f9810ea640
Tine Jozelj <tine.jozelj@tjo.space>
2017-12-17 19:23:02 +0100

    add support for materials - 1.0.3

--- lib/index.spec.ts
+++ lib/index.spec.ts
@@ -2,23 +2,58 @@ import compiler from '../test/compiler';
 import * as fs from 'fs';
 import * as path from 'path';
 
-const file = name => path.resolve(__dirname, `../test/${name}.obj`);
+const file = name => path.resolve(__dirname, `../test/${name}`);
 const fileContents = name => fs.readFileSync(file(name)).toString();
 
 import { Mesh } from 'webgl-obj-loader';
 
-describe('Test loading', () => {
-  test('example.obj', async () => {
-    const data = (await compiler(file('example'))).toJson();
+describe('Test loading With Material', () => {
+  test('example-w-material.obj', async () => {
+    const data = (await compiler(file('example-w-material.obj'))).toJson();
 
     if (data.errors && data.errors.length > 0) throw Error(data.errors[0]);
 
     const output = data.modules[0].source;
 
-    expect(output).toBe(`
-  import { Mesh } from 'webgl-obj-loader';
-  const source = ${JSON.stringify(fileContents('example'))};
+    expect(output.replace(/\s/g, '')).toBe(`
+  import { Mesh, Material } from 'webgl-obj-loader';
+  const source = ${JSON.stringify(fileContents('example-w-material.obj'))};
   const mesh = new Mesh(source);
-  export default mesh;`);
+  const materialSource = ${JSON.stringify(fileContents('example-w-material.mtl'))};
+  const materialLibrary = new MaterialLibrary(materialSource);
+  mesh.addMaterialLibrary(materialLibrary);
+  export default mesh;`.replace(/\s/g, ''));
+  });
+});
+
+describe('Test loading Without Material', () => {
+  test('example.obj', async () => {
+    const data = (await compiler(file('example.obj'))).toJson();
+
+    if (data.errors && data.errors.length > 0) throw Error(data.errors[0]);
+
+    const output = data.modules[0].source;
+
+    expect(output.replace(/\s/g, '')).toBe(`
+  import { Mesh, Material } from 'webgl-obj-loader';
+  const source = ${JSON.stringify(fileContents('example.obj'))};
+  const mesh = new Mesh(source);
+  export default mesh;`.replace(/\s/g, ''));
+  });
+});
+
+describe('Test loading Missing Material', () => {
+  test('example-m-material.obj', async () => {
+    const data = (await compiler(file('example-m-material.obj'))).toJson();
+
+    if (data.errors && data.errors.length > 0) throw Error(data.errors[0]);
+
+    const output = data.modules[0].source;
+
+    expect(output.replace(/\s/g, '')).toBe(`
+  import { Mesh, Material } from 'webgl-obj-loader';
+  const source = ${JSON.stringify(fileContents('example-m-material.obj'))};
+  const mesh = new Mesh(source);
+  export default mesh;`.replace(/\s/g, ''));
   });
 });
--- lib/index.ts
+++ lib/index.ts
@@ -1,9 +1,50 @@
-export default function (source) {
-  this.cacheable();
+import * as fs from 'fs';
+
+import { MaterialLibrary } from 'webgl-obj-loader';
+
+const getFile = file => new Promise((resolve, reject) => fs.readFile(
+  file,
+  (err, data) => err ? reject(err) : resolve(data.toString())));
+
+const existsFile = file => new Promise(resolve => fs.exists(file, resolve));
+
+async function getTextures(path, textures) {
+
+}
+
+async function getMaterial(path: string): Promise<string> {
+  const mtlFile = await getFile(path);
+
+  // TODO: Maybe load textures here?
 
   return `
-  import { Mesh } from 'webgl-obj-loader';
+    const materialSource = ${JSON.stringify(mtlFile)};
+    const materialLibrary = new MaterialLibrary(materialSource);
+    mesh.addMaterialLibrary(materialLibrary);
+    `;
+}
+
+export default function (source: string): void {
+  this.cacheable();
+  this.async();
+
+  const run = async () => {
+    let additional = '';
+
+    const mtlPath = this.resourcePath.replace(/\.obj$/, '.mtl');
+    console.log(mtlPath);
+
+    if (await existsFile(mtlPath)) additional += await getMaterial(mtlPath);
+
+    this.callback(null, `
+  import { Mesh, Material } from 'webgl-obj-loader';
   const source = ${JSON.stringify(source)};
   const mesh = new Mesh(source);
-  export default mesh;`;
+  ${additional}
+  export default mesh;`);
+  };
+
+  run().catch((err) => {
+    throw err;
+  });
 }
